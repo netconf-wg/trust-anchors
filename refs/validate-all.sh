cf803cd3760d67f10a908e0e316ebfda88b82fe4
kwatsen <kent@watsen.net>
2020-02-04 17:28:12 -0500

    adding support for bags

--- refs/validate-all.sh
+++ refs/validate-all.sh
@@ -8,7 +8,7 @@ echo "Testing ietf-truststore.yang (yanglint)..."
 yanglint ../ietf-truststore\@*.yang
 
 echo "Testing ex-truststore.xml..."
-yanglint -s ../ietf-truststore\@*.yang ./ietf-origin.yang ex-truststore.xml
+yanglint -s ../ietf-truststore\@*.yang ../ietf-crypto-types\@*.yang ./ietf-origin.yang ex-truststore.xml
 
 echo "Testing ex-notification-ce.xml..."
 echo -e 'setns a=urn:ietf:params:xml:ns:netconf:notification:1.0\nsetns b=urn:ietf:params:xml:ns:yang:ietf-truststore\ncat //a:notification/b:truststore' | xmllint --shell ex-notification-ce.xml | sed -e '/^\/.*/d' -e '/^ *$/d' > yanglint-notification.xml
